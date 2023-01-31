dfa675c0461b0b14e3e3b600d4b9f66ffa9faae4
= <=>
2023-01-30 21:55:53 -0500

    changes function sequelize, add localstorage local

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,42 @@ const express = require('express');
 const router = express.Router();
 const sequelize = require('../conecction');
 const { translate } = require('@vitalets/google-translate-api');
+const {success, error} = require("../utils/customResponse");
+const { LocalStorage } = require('node-localstorage');
+const localStorage = new LocalStorage('./scratch');
+const { QueryTypes } = require('sequelize');
+const moment = require('moment');
 
 router.get('/', async (req, res, next) => {
-  const { user, pass } = req.query;
+  const password = localStorage.getItem('password');
+  const username = localStorage.getItem('username');
   try {
-    const dat = await sequelize(user, pass).query('select * from dbo.Artist');
-    res.render('index', {
-      title: 'Hola'
-    })
+    if (username.length > 0 && password.length > 0) res.redirect('/dashboard');
+    else res.redirect('/login');
   } catch (e) {
     const text = await translate(e.message, {to: 'es',from: 'en'});
-    console.log(text);
   }
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', {
-    title: 'ClydeMusic4All',
-  });
+  res.render('login');
+});
+
+router.post('/sign-in', async (req, res) => {
+  const { user, password } = req.body;
+
+  try {
+    await sequelize(user, password).authenticate();
+    localStorage.setItem('username', user);
+    localStorage.setItem('password', password);
+    return res.render('index', {
+      title: 'Ingreso exitoso'
+    });
+  } catch (e) {
+    return res.render('login', {
+      error: e.message,
+    });
+  }
 });
 
 router.post('/newUSer', async (req, res) => {
@@ -36,6 +54,41 @@ router.post('/newUSer', async (req, res) => {
       res.send(response.text).status(203);
     }).catch(err => {
       res.send(e.message).status(203);
+    });
+  }
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('index', {
+    title: 'Bienvenido',
+  });
+});
+
+router.get('/databases', async (req, res) => {
+  const password = localStorage.getItem('password');
+  const username = localStorage.getItem('username');
+
+  try {
+    const databases = await sequelize(username, password)
+      .query(
+        `select * from sys.databases order by create_date desc`
+        , {
+          type: QueryTypes.SELECT,
+        });
+
+    const dataFormat = databases.map(item => ({
+      ...item,
+      create_date: moment(item.create_date).format('DD/MM/yyyy'),
+    }));
+
+    res.render('databases', {
+      title: 'Bases de datos',
+      databases: dataFormat || [],
+    });
+  } catch (e) {
+    res.render('databases', {
+      title: 'Bases de datos',
+      message: e.message,
     });
   }
 });
